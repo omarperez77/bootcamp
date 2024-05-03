cf9099b91c82fb2d9a66e77b8d8fb22fcf152cac
Omar Perez <omarg.perezm@gmail.com>
2024-05-03 09:00:19 -0600

    funcionando al 100

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Chirp;
+	// Add this line
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/chirps', [ChirpController::class, 'index'])->name('chirps.index');
+
+Route::post('/chirps', [ChirpController::class, 'store'])->name('chirps.store');
+
+Route::get('/dasreturnhboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
